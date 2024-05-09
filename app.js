63ea4942227c164982c6259f2633de18eecd41b0
foley61 <coder61616@gmail.com>
2024-05-09 18:00:15 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const nameSpn = document.querySelector(".name")
+const emailSpn = document.querySelector(".email")
+const phoneSpn = document.querySelector(".phone")
+const pictureSrc = document.querySelector(".pictureSrc")
+const API = "https://randomuser.me/api/";
+
+const getData = ()=>{
+    fetch(API)
+      .then((res) => { 
+        if(!res.ok){
+            throw new error("Something went wrong",res.status)
+        }
+        return res.json()
+    })
+    .then((data) => showData(data))
+    .catch((err) => console.log(err,res.status))
+}
+
+const showData = (data) => {
+    const {name ,email,phone,picture} = data.results[0]
+
+   
+    let {title,first,last} = name
+
+    nameSpn.textContent = title + " " + first + " " +  last
+    emailSpn.textContent = email
+    phoneSpn.textContent = phone
+    pictureSrc.setAttribute("src",`${picture.large}`)
+}
+getData()    
